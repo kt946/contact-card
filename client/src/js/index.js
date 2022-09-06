662b255a2375ab54916c0880240a4b7bb7faf488
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-06 16:53:38 -0400

    rendered indexddb database data on cards

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,7 @@
 // Import modules
-import  "./form";
-import "./submit";
+import { toggleForm, clearForm } from "./form";
 import { initdb, getDb, postDb } from './database';
+import { fetchCards } from './cards';
 
 // Import Bootstrap and Popper
 import { Tooltip, Toast, Popover } from 'bootstrap';
@@ -18,11 +18,44 @@ import Dog from '../images/dog.png';
 // Add images on load
 window.addEventListener('load', function () {
   initdb();
-  // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
-  // getDb();
-  // postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
-  // getDb();
+  fetchCards();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
+});
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+})
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let email = document.getElementById("email").value;
+let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+if (submitBtnToUpdate == false) {
+  postDb(name, email, phone, profile);
+} else {
+
+  fetchCards();
+    // Toggles the submit button back to POST functionality
+  submitBtnToUpdate = false;
+}
+
+// Clear form
+clearForm();
+// Toggle form
+toggleForm();
+// Reload the DOM
+fetchCards();
 });
