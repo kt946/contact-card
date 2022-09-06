fc2ead5d971f6dd4a00a8acab95bf4dd4214c2cb
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-09-06 17:46:39 -0400

    added update functionality for indexddb data on a card

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,7 +3,7 @@ import 'regenerator-runtime/runtime';
 
 export const initdb = async () => {
   // We are creating a new database named 'contact_db' which will be using version 1 of the database.
-  const db = await openDB('contact_db', 1, {
+  await openDB('contact_db', 1, {
     // Add our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('contacts')) {
@@ -73,11 +73,32 @@ export const deleteDb = async (id) => {
   // Open up the desired object store.
   const store = tx.objectStore('contacts');
 
-  // Use the .delete() method to get all data in the database.
+  // Use the .delete() method to delete data in the database.
   const request = store.delete(id);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
   return result?.value;
+};
+
+// EXPORTED EDIT function
+export const editDb = async (id, name, email, phone, profile) => {
+  console.log('PUT to the database');
+
+  // Create a connection to the IndexedDB database and the version we want to use.
+  const contactDb = await openDB('contact_db', 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction('contacts', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('contacts');
+
+  // Use the .put() method to update data in the database.
+  const request = store.put({ id: id, name: name, email: email, phone: phone, profile: profile });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 };
